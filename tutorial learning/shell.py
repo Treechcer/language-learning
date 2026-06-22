06ca76893821eb397f0e2962994ff67e74a6f66e
treecher <treechcer@gmail.com>
2026-06-22 21:04:14 +0200

    part of episode 2, there's mistake somehwere

--- tutorial learning/shell.py
+++ tutorial learning/shell.py
@@ -1,7 +1,10 @@
 import basic
 
 while True:
-    text = input("baic > ")
+    text = input("basic > ")
+    # Found it annoying to press ctrl c, so I've made this
+    if text == "q":
+        exit()
     result, err = basic.run("<stdin>", text)
 
     if err: print(err.as_string())
